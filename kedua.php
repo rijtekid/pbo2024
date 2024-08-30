d7176386da03c97d95d5299bc9e6ecf904aac6fe
rijtehkid <rizqkr@gmail.com>
2024-08-30 11:56:38 +0700

    Praktikum 4B - Membuat Atribut

--- kedua.php
+++ kedua.php
@@ -3,46 +3,51 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
-
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function luas(): float {
+        return self::PHI * $this->jari_jari * $this->jari_jari;
     }
 
-
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function keliling(): float {
+        return 2 * self::PHI * $this->jari_jari;
     }
 }
-
 
 class Bola
 {
     const PHI = 3.14;
-    public function volume($jari_jari) : float {
-        return (4/3)*self::PHI*pow($jari_jari,3);
+    public $jari_jari;
+
+    public function volume(): float {
+        return (4 / 3) * self::PHI * pow($this->jari_jari, 3);
     }
 }
-
 
 class Tabung
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+
+    public function volume($tinggi): float {
+        return self::PHI * pow($this->jari_jari, 2) * $tinggi;
     }
 }
-
 
 class Kerucut
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+
+    public function volume($tinggi): float {
+        return (1 / 3) * self::PHI * pow($this->jari_jari, 2) * $tinggi;
     }
 }
 
-$nasi_tumpeng = (new Kerucut())->volume(4,10);
+$kerucut = new Kerucut();
+$kerucut->jari_jari = 4; 
 
-echo "Volume nasi tumpeng adalah {$nasi_tumpeng} cm³"
+$nasi_tumpeng = $kerucut->volume(10);
+
+echo "Volume nasi tumpeng adalah {$nasi_tumpeng} cm³";
 ?>
