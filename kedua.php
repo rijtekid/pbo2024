05302bc8fedece6efbb317bd7f660d76371e09fe
rijtehkid <rizqkr@gmail.com>
2024-08-30 12:45:38 +0700

    Praktikum 4B - Membuat Atribut

--- kedua.php
+++ kedua.php
@@ -44,10 +44,28 @@ class Kerucut
     }
 }
 
+$lingkaran = new Lingkaran();
+$lingkaran->jari_jari = 4;
+
 $kerucut = new Kerucut();
 $kerucut->jari_jari = 4; 
 
-$nasi_tumpeng = $kerucut->volume(10);
+$bola = new Bola();
+$bola->jari_jari = 4;
 
-echo "Volume nasi tumpeng adalah {$nasi_tumpeng} cm³";
+$tabung = new Tabung();
+$tabung->jari_jari = 4;
+
+$luastanah = $lingkaran->luas();
+$kelilingtanah = $lingkaran->keliling();
+$nasi_tumpeng = $kerucut->volume(10);
+$bola_angin = $bola->volume();
+$botol = $tabung->volume(10);
+
+echo "Luas tanah adalah {$luastanah} m² \n";
+echo "Keliling tanah adalah {$kelilingtanah} m² \n";
+echo "Volume nasi tumpeng adalah {$nasi_tumpeng} cm³ \n";
+echo "Volume bola adalah {$bola_angin} cm³ \n";
+echo "Volume botol adalah {$botol} cm³ \n";
+
 ?>
