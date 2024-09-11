ea2c924c148a075532b4bab10c8c3e8856cf8ccd
rijtehkid <rizqkr@gmail.com>
2024-09-11 14:52:48 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- keempat.php
+++ keempat.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once 'Author.php';
+require_once 'Book.php';
+require_once 'Publisher.php';
+
 $author = new Author("John Doe Slamet", "Famous indonesian author");
 $book = new Book("978-7-997610-0", "Mencari jati diri", "Buku ini bagus", "Fiction", "English", 200, $author, "Kompas Gramedia");
 $publisher = new Publisher("ABC Publishers", "123 Main St");
